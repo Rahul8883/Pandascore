9f43c0d8eca70908b720f56d102ce0854c109879
Rahul Ranjan <rahul.ranjan@office365.3pillarglobal.com>
2020-07-17 13:12:22 +0530

    add

--- pandascore-new/src/App.js
+++ pandascore-new/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import  Home  from "./component/home";
 import Login from './component/login';
 import DashboardComp from './component/dashboard.component';
